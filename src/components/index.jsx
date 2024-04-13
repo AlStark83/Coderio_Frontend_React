1900222a09c46536ac66ae126c480abf5568d072
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 13:51:34 -0600

    index for Components and Views created

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import Button from "./Button";
+import Card from "./Card";
+import Header from "./Header";
+// import Footer from "./Footer";
+import Loader from "./Loader"
+import Logo from "./Logo";
+// import  Modal from "./Modal";
+import NavBar from "./NavBar";
+import Pagination from "./Pagination";
+import ProductsGrid from "./ProductsGrid";
+
+export {Banner, Button, Card, Header, /*Footer,*/ Loader, Logo, /*Modal, */NavBar, Pagination, ProductsGrid}
