ce7e916908e06a82f288bb0a3288f08ebd529851
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 12:18:36 -0600

    Home Component created

---
+++ src/view/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../../components/Banner";
+import ProductsGrid from "../../components/ProductsGrid";
+// import { useEffect, useState } from
+function Home() {
+	return (
+		<>
+			<Banner />
+			<h1>Home</h1>
+			<ProductsGrid />
+		</>
+	);
+}
+
+export default Home;
