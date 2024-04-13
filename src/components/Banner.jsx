5b51d1c137c40510b68e7616690b698cb5f8dbea
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 12:06:55 -0600

    Banner Component created

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+function Banner() {
+	return (
+		<>
+			<div className="banner">
+				<h1>Bienvenido a nuestra tienda en línea</h1>
+				<p>Descubre nuestras ofertas especiales</p>
+			</div>
+		</>
+	);
+}
+
+export default Banner;
