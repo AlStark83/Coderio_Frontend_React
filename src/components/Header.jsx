6a48c4bf46b4d1e457a5af2ab7f0d29c7617d168
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 12:19:57 -0600

    Header Component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from './Logo'
+import NavBar from './NavBar'
+import Button from './Button'
+function Header() {
+  return (
+    <>
+    <Logo/>
+    <NavBar/>
+    <Button/>
+    </>
+  )
+}
+
+export default Header
