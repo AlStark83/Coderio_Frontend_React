a3e7ac30ab66a134356d93be8deabc9712ae0b64
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 12:19:21 -0600

    Nav Bar Component created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+function NavBar() {
+  return (
+    <>
+     <nav>
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/productos">Productos</a></li>
+          <li><a href="/ofertas">Ofertas</a></li>
+          <li><a href="/contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
