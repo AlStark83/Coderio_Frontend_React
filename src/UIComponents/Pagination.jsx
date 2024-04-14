7eb5b9a1ab775205f9535b3064f4b47d970e3491
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-14 09:02:27 -0600

    Logic of Pagination ok

--- src/UIComponents/Pagination.jsx
+++ src/UIComponents/Pagination.jsx
@@ -1,9 +1,24 @@
-function Pagination() {
+
+
+
+// eslint-disable-next-line react/prop-types
+function Pagination({ productsPerPage, allProducts, pagination }) {
+
+	const pageNumbers = [];
+		
+	for (let i = 0; i < Math.ceil(allProducts/ productsPerPage); i++) {
+		pageNumbers.push(i+1);
+	}
+
 	return (
-		<>
-			<h4>Pagination</h4>
-    
-    </>
+		<nav >
+			<div  className="paginado">
+				{pageNumbers &&
+					pageNumbers.map((number) => (
+							<button key={number} className="button" onClick={() => pagination(number)}>{number}</button>
+					))}
+			</div>
+		</nav>
 	);
 }
 export default Pagination;
--- src/UIComponents/ProductsGrid.jsx
+++ src/UIComponents/ProductsGrid.jsx
@@ -3,7 +3,19 @@ import Pagination from "./Pagination";
 import { useEffect, useState } from "react";
 
 function ProductsGrid() {
+	const [currentPage, setCurrentPage] = useState(1); 
+	// eslint-disable-next-line no-unused-vars
+	const [productsPerPage, setProductsPerPage] = useState(4);
+	const indexOfLastProduct = currentPage * productsPerPage; 
+	const indexOfFirtsProduct = indexOfLastProduct - productsPerPage; 
 	const [data, setData] = useState("");
+	const currentProducts = data.slice(
+		indexOfFirtsProduct,
+		indexOfLastProduct
+	);
+	const pagination = (number) => {
+		setCurrentPage(number);
+	};
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products")
 			.then((res) => res.json())
@@ -12,8 +24,8 @@ function ProductsGrid() {
 	}, []);
 	return (
 		<>
-			{data &&
-				data.map((el) => {
+			{currentProducts &&
+				currentProducts.map((el) => {
 					return (
 						<li key={el.id} className={"producto"}>
 							<Card
@@ -25,7 +37,9 @@ function ProductsGrid() {
 						</li>
 					);
 				})}
-			<Pagination />
+			<Pagination productsPerPage={productsPerPage}
+								allProducts={data.length}
+								pagination={pagination}/>
 		</>
 	);
 }
