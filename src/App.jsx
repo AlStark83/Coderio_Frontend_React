72fc7131e3b7e40ee2b847e4e740545ec577439a
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-13 22:56:56 -0600

    Login route deleted, imports updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, Login } from "./view/index";
-import { Header } from "./components/index";
+import Header  from "./UIComponents/Header";
+import  ModalLoginForm  from "./Auth/ModalLoginForm";
+import  AuthHandler  from "./Auth/AuthHandler";
+import { Home, CheckOut } from "./view/index";
 
 function App() {
+
 	return (
 		<Router>
 			<>
 				<Header />
+				
+				<ModalLoginForm />
+				<AuthHandler/>
 				<Routes>
-					<Route path="/" element={<Home/>} />
-					<Route path="/login" element={<Login/>} />
+					<Route path="/" element={<Home />} />
+					<Route path="/checkout" element={<CheckOut />} />
 				</Routes>
 			</>
 		</Router>
