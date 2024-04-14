3509562e10c3bab9ddb99be88486378a9f1afb3d
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-14 14:02:28 -0600

    Card props adjust for redux state

--- src/UIComponents/Card.jsx
+++ src/UIComponents/Card.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable react/prop-types */
-// import Link  from 'react'
-function Card({ id, name, image, title, price }) {
-  return (
-    <>
-    <div className={"id"} key={id}>
-			{/* <Link to={`/product/${id}`}> */}
-				<img className={"image"} src={image} alt="not found" />
-			{/* </Link> */}
-			<h2>{title}</h2>
-			<h2>{name}</h2>
-			<h3 className={price}>{price}</h3>
-		</div>
-    </>
-  )
+import ButtonAddToCartConnected from "./ButtonAddToCart";
+
+function Card({ product }) {
+	return (
+		<>
+			<div className="product-card" key={product.id}>
+				<img className="product-image" src={product.image} alt="not found" height="200px"  width="250px" />
+				<h2>{product.title}</h2>
+				<h3 className="product-price">{product.price}</h3>
+				<ButtonAddToCartConnected product={ product } />
+			</div>
+		</>
+	);
 }
 
-export default Card
+export default Card;
