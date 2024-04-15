255d6ceed48e75ada7c68ddedab607ad60409620
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-15 15:45:48 -0600

    coments deleted

--- src/UIComponents/NavBar.jsx
+++ src/UIComponents/NavBar.jsx
@@ -6,8 +6,6 @@ function NavBar() {
       <nav className=" md:flex flex-grow justify-center w-2/4 h-8">
     
         <Link className="text-white px-4 py-2 text-sm font-medium transition-all hover:text-gray-200 hover:font-semibold hover:text-lg" to="/">Products</Link>
-          {/* <li><Link to="/productos">Productos</Link></li>
-          <li><Link to="/ofertas">Ofertas</Link></li> */}
         <Link className="text-white px-4 py-2 text-sm font-medium transition-all hover:text-gray-200 hover:font-semibold hover:text-lg" to="/cart">Cart</Link>
       
       </nav>
