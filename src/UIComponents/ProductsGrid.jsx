f1a5a35a03d38b2c7f1fce780350bc6d7e66c0d1
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-15 15:45:20 -0600

    coments deleted and useState deleted in constant productsPerPage

--- src/UIComponents/ProductsGrid.jsx
+++ src/UIComponents/ProductsGrid.jsx
@@ -4,8 +4,7 @@ import { useEffect, useState } from "react";
 
 function ProductsGrid() {
 	const [currentPage, setCurrentPage] = useState(1);
-	// eslint-disable-next-line no-unused-vars
-	const [productsPerPage, setProductsPerPage] = useState(4);
+	const productsPerPage= 4;
 	const indexOfLastProduct = currentPage * productsPerPage;
 	const indexOfFirtsProduct = indexOfLastProduct - productsPerPage;
 	const [data, setData] = useState("");
@@ -16,7 +15,6 @@ function ProductsGrid() {
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products")
 			.then((res) => res.json())
-			// .then((json) => console.log(json))
 			.then((data) => setData(data));
 	}, []);
 	return (
@@ -28,10 +26,6 @@ function ProductsGrid() {
 							<li key={el.id} className="w-full md:w-1/2 lg:w-1/4 list-none">
 								<Card
 									product={el}
-									// id={el.id}
-									// image={el.image}
-									// title={el.title}
-									// price={el.price}
 								/>
 							</li>
 						);
