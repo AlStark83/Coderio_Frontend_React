a31eb81f6c65b386bfeb575bd851132513c3471a
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-15 15:41:25 -0600

    coments deleted

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,11 +1,10 @@
 import { Banner, ProductsGrid } from "../../UIComponents/index";
 import  SideCart  from "../../UIComponents/SideCart";
-// import { useEffect, useState } from
+
 function Home() {
 	return (
 		<>
 			<Banner />
-			{/* <h1>Home</h1> */}
 			<div className="align-center">
 			<ProductsGrid />
 			</div>
