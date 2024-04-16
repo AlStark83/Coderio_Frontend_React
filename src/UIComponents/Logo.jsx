1dbd408fab2ea0987b1497e04fa1e2e78e6b4537
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-04-15 23:52:28 -0600

    Link to "/" added on the Logo

--- src/UIComponents/Logo.jsx
+++ src/UIComponents/Logo.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Logo() {
   return (
+    <Link to={'/'}>
     <img src="https://albertoguerrero.site/assets/Logo.png" alt="Logo" className="ml-2 h-10 w-auto" />
+    </Link>
   );
 }
 
